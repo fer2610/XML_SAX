96e681d6c922e4b77eb1b5cefffdbfbee592dc08
Fernando Torres <a219210879@unison.mx>
2022-05-07 11:13:30 -0700

    proyecto inicial

---
+++ src/main/java/main/java/saxdemo/BreakFastHandler.java
@@ -0,0 +1,131 @@
+package main.java.saxdemo;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class BreakFastHandler extends DefaultHandler {
+
+    private static final String CLASS_NAME = DisplayXML.class.getName();
+    private final static Logger LOG = Logger.getLogger(CLASS_NAME);
+
+    private boolean name;
+    private boolean price;
+    private boolean description;
+    private boolean calories;
+
+    private Food fooItem;
+    private ArrayList<Food> lista;
+
+    public BreakFastHandler() {
+        super();
+
+        lista = new ArrayList<>();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName,
+                             Attributes attributes) throws SAXException {
+        //LOG.info("startElement");
+
+        if (localName.equals("food")) {
+            fooItem = new Food();
+        }
+        if (localName.equals("name"))
+            name = true;
+        if (localName.equals("price"))
+            price = true;
+        if (localName.equals("description"))
+            description = true;
+        if (localName.equals("calories"))
+            calories = true;
+    }
+
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        // LOG.info("characters");
+        String contenido = new String(ch, start, length);
+
+        if (name) {
+            fooItem.setName(contenido);
+        } else if (price) {
+            fooItem.setPrice(Double.parseDouble(contenido));
+        } else if (description) {
+            fooItem.setDescription(contenido);
+        } else if (calories) {
+            fooItem.setCalories(Integer.parseInt(contenido));
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        //LOG.info( qName );
+        if (localName.equals("name"))
+            name = false;
+        if (localName.equals("price"))
+            price = false;
+        if (localName.equals("description"))
+            description = false;
+        if (localName.equals("calories"))
+            calories = false;
+
+        if ( localName.equals("food") ) {
+            //System.out.println( fooItem.toString() ) ;
+            lista.add( fooItem );
+        }
+    }
+
+    public ArrayList<Food> getLista() {
+        return lista;
+    }
+
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            LOG.severe("No file to process. Usage is:" + "\njava BreakFastHandler <filename>");
+            return;
+        }
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        factory.setValidating(true);
+
+        SAXParser saxParser = null;
+        try {
+            saxParser = factory.newSAXParser();
+        } catch (ParserConfigurationException e) {
+            LOG.severe(e.getMessage());
+        } catch (SAXException e) {
+            LOG.severe(e.getMessage());
+        }
+
+        File xmlFile = new File(args[0]);
+
+        BreakFastHandler handler = new BreakFastHandler();
+
+        try {
+            saxParser.parse(xmlFile, handler);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            LOG.severe(e.getMessage());
+        }
+
+        ArrayList<Food> lista = handler.getLista();
+
+        for (Food food: lista) {
+            System.out.println( food.toString() );
+        }
+
+
+    }
+}
---
+++ src/main/java/main/java/saxdemo/Food.java
@@ -0,0 +1,45 @@
+package main.java.saxdemo;
+
+public class Food {
+    private String name;
+    private double price;
+    private String description;
+    private int calories;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-25.25s, $%4.2f (%d calorias) \n\t%-80.80s",name, price, calories, description);
+    }
+}
